04ee27897ba1dcac20c966d1378f5e36b13e2daa
Edward Shih <ultralabed@gmail.com>
2016-10-07 11:44:09 +0800

    Add route and navigator.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
 let Parse = require('parse/react-native');
 let Signin = require('./components/authentication/signin');
+
+const ROUTES = {
+  signin: Signin
+};
 
 module.exports = React.createClass({
   componentWillMount: function() {
     Parse.initialize("epHqnJOxMHEKGloGBgZCpn6SIKHdxX5q97a5iVMj", "uZXxWDuXj9msgTzSQAtPKYmWnBWooKGPsU5k7Bp4");
   },
+  renderScene: function(route, navigator) {
+    let Component = ROUTES[route.name];
+    return <Component />
+  },
   render: function() {
     return (
-      <View style={ styles.container }>
-        <Signin/>
-      </View>
+      <Navigator 
+        style={ styles.container }
+        initialRoute={ {name: 'signin'} }
+        renderScene={ this.renderScene }
+        configureScene={ () => { return Navigator.SceneConfigs.FloatFromRight; } }
+        />
     )
   }
 });
@@ -23,7 +33,5 @@ module.exports = React.createClass({
 let styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 });
